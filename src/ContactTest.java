1780de456a154ed1175f32001ee5d0b72efe220f
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-05-18 09:47:09 -0500

    feat: getters and setters completed

---
+++ src/ContactTest.java
@@ -0,0 +1,25 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ContactTest {
+    String directory = "data";
+    String filename = "contacts.txt";
+    Path dataDirectory = Paths.get(directory);
+
+
+
+    public static void main(String[] args) {
+
+//        ContactsList contact1 = new ContactsList("Nayo","Kim","123-455-9860");
+//
+//        System.out.println(contact1.getFirstName());
+//
+//    }
+
+
+
+
+}
+
+
+
---
+++ src/ContactsList.java
@@ -0,0 +1,43 @@
+public class ContactsList {
+    String firstName;
+    String lastName;
+    String phoneNumber;
+
+    //constructor
+    public ContactsList(String firstName, String lastName, String phoneNumber){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    //getter
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+    //setter
+    public void setFirstName(String firstName){
+        this. firstName = firstName;
+    }
+
+    public String getLastName(){
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber(){
+        return this.phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+
+
+
+
+}
